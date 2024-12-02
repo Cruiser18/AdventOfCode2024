e68afcd512a599332bbbe52267ac98227c4525f6
Christian Sunesen <christian_sunesen@hotmail.com>
2024-12-02 18:26:45 +0100

    Finished first start day 2

--- Day2/Solution.fsx
+++ Day2/Solution.fsx
@@ -2,30 +2,49 @@ open System.IO
 open System
 
 let lines = 
-    File.ReadAllLines("Day2/TestInput.txt")
+    File.ReadAllLines("Day2/Input.txt")
     |> Array.toList
     |> List.map (fun x -> 
         x.Split(' ')
         |> Array.map (fun x -> int(x))
+        |> Array.toList
     )
 
 type IsSafe = Safe | Unsafe
-type determineSafety = List<int> -> bool -> IsSafe
 
-let rec determineSafety (line: List<int>) wasIncrement =
-    match wasIncrement with
+let getDifference x y = 
+    match x with
+    | x when x > y -> x - y
+    | _ -> y - x
+
+let shouldIncrement x =
+    match x with
+        | x::xs when x > xs.Head -> false
+        | _ -> true
+
+let rec determineSafety isIncrementing (line: List<int>) =
+    printfn "%A" line
+    match isIncrementing with
     | true ->
         match line with
+        | [_] -> Safe
         | [] -> Safe
-        | x::xs when (x + xs.Head) < 1  -> Unsafe
-        | x::xs when (x + xs.Head) > 3 -> Unsafe
+        | x::xs when (shouldIncrement [x;xs.Head]) = false -> Unsafe
+        | x::xs when (getDifference x xs.Head) < 1  -> Unsafe
+        | x::xs when (getDifference x xs.Head) > 3 -> Unsafe
+        | _::xs -> determineSafety isIncrementing xs
     | false ->
         match line with
+        | [_] -> Safe
         | [] -> Safe
-        | x::xs when (x - xs.Head) < 1  -> Unsafe
-        | x::xs when (x - xs.Head) > 3 -> Unsafe
-    
-        
+        | x::xs when (shouldIncrement [x;xs.Head]) = true -> Unsafe
+        | x::xs when (getDifference x xs.Head) < 1  -> Unsafe
+        | x::xs when (getDifference x xs.Head) > 3 -> Unsafe
+        | _::xs -> determineSafety isIncrementing xs
 
 let result = 
-    determineSafety [1;2;3;4] false
+    List.map (fun x -> determineSafety  (shouldIncrement x) x) lines
+    |> List.filter (fun x -> x = Safe)
+    |> List.length
+
+// Answer was 483
